c77b133874fb3b0b1119a12df6bef486798e550c
heejun32 <73517978+heejun32@users.noreply.github.com>
2021-11-05 23:11:43 +0900

    c

--- Week5/Biggest_Number.py
+++ Week5/Biggest_Number.py
@@ -1,12 +1,13 @@
 def solution(numbers):
+    answer = ''
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
+    numbers.sort(key=lambda x : 3 * x, reverse=True)
     answer = str(int(''.join(numbers)))
-    return answer
-
-
+    return answera
 
 print(solution([6, 10, 2]))
 
-### 다시 공부하기!
-https://velog.io/@younge/Python-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B0%80%EC%9E%A5-%ED%81%B0-%EC%88%98-%EC%A0%95%EB%A0%AC
+# 아스키 코드로 문자열 값들을 비교내 내림차순 정리! 아스키 값이 큰 문자열이 앞으로 올수록, 제일 높은 자리의 숫자가 커지는 것이기 때문에 내림차순임!
+# '321' '3'이 있을때 아스키 코드 비교시 앞자리 3만 비교해서 우위를 판단할 수 없음. 그래서 문제 조건인 각 숫자는 3자리 이하다라는 것을 활용
+# 임의로 '3'을 3자리로 늘려 '333' '321'을 비교해 3이 321보다 먼저 정렬되게 작성한것임!
+# 마지막 int()는 0 리스트 받을시의 예외처리임!
