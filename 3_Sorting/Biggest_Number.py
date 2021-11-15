edb2b49aec7ef874be3f64edc0c94ab6f28512bf
heejun32 <73517978+heejun32@users.noreply.github.com>
2021-11-15 15:47:35 +0900

    c

--- 3_Sorting/Biggest_Number.py
+++ 3_Sorting/Biggest_Number.py
@@ -3,7 +3,7 @@ def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x : 3 * x, reverse=True)
     answer = str(int(''.join(numbers)))
-    return answera
+    return answer
 
 print(solution([6, 10, 2]))
 
