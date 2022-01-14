11a869d214a0281142ec8b6d038e6c157d3b2802
heejun32 <rlaskan95@gmail.com>
2022-01-14 21:49:56 +0900

    2108

--- 9_Mathmatical/2108.py
+++ 9_Mathmatical/2108.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 numbers = []
 for _ in range(N):
@@ -7,23 +10,32 @@ for _ in range(N):
 numbers.sort()
 mean = round(sum(numbers) / len(numbers))
 middle = numbers[len(numbers) // 2]
-scope = numbers[-1] - numbers[0]
-
+scope = abs(numbers[-1] - numbers[0])
 mfreq = 0
-my_dict = {}
-for i in numbers:
-    if i not in my_dict.keys():
-        my_dict[i] = 1
-    else:
-        my_dict[i] += 1
 
-frequences = list(my_dict.values())
-print(frequences)
-mfreq2 = frequences.sort()[-2]
-for key, value in my_dict:
-    if value == mfreq2:
-        mfreq = key
-        break
+from collections import Counter
+
+# f_dict = {}
+# for i in numbers:
+#     if i in f_dict.keys():
+#         f_dict[i] += 1
+#     else:
+#         f_dict[i] = 1
+#
+# f_list = []
+# for key, value in f_dict.items():
+#     f_list.append([key, value])
+# f_list.sort(key=lambda x: (-x[1], x[0]))
+
+f_list = Counter(numbers).most_common(2)
+print(f_list)
+if len(f_list) > 1:
+        if f_list[0][1] != f_list[1][1]:
+            mfreq = f_list[0][0]
+        else:
+            mfreq = f_list[1][0]
+else:
+    mfreq = f_list[0][0]
 
 print(mean)
 print(middle)
