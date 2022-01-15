8e1d550971cd5c5c965ac4a284ccde75d3003e17
heejun32 <rlaskan95@gmail.com>
2022-01-15 23:01:05 +0900

    1874 1181 10814 2751

--- 10_Implementation/1181.py
+++ 10_Implementation/1181.py
@@ -7,18 +7,8 @@ for i in range(N):
         continue
     else:
         words.append(word)
-# Tim sort를 공부한 뒤 sorted를 사용할 것! 병합정렬 공부 뒤에 이 문제는 다시 풀기
-for i in range(len(words) - 1):
-    for j in range(i + 1, len(words)):
-        if len(words[i]) > len(words[j]):
-            temp = words[i]
-            words[i] = words[j]
-            words[j] = temp
 
-        if len(words[i]) == len(words[j]) and words[i] > words[j]:
-            temp = words[i]
-            words[i] = words[j]
-            words[j] = temp
+words.sort(key=lambda x : (len(x), x))
 
 for word in words:
     print(word)
