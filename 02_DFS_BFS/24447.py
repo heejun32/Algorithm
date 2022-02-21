a2c0a779d35dbbfbd167176cd62b061fac668bc6
heejun32 <rlaskan95@naver.com>
2022-02-22 00:08:44 +0900

    24446 24447 24479

--- 02_DFS_BFS/24447.py
+++ 02_DFS_BFS/24447.py
@@ -1,43 +1,51 @@
 from collections import deque
-from pickletools import long1, long4
 import sys
 input = sys.stdin.readline
 queue = deque()
 
-def bfs(V, E, R, O):
-    V[R] = 0
+# bfs 함수
+def bfs(V, E, R, D, O):
+    V[R] = 1            # 방문 확인
+    D[R] = 0            # 깊이 설정
     O[R] = 1
     depth = 0
     order = 1
     queue.append(E[R])
     
     while queue:
-        u = queue.popleft()
-        u.sort()
         depth += 1
-        for edge in u:
-            if V[edge] == -1:
-                V[edge] = depth
-                order += 1
-                O[edge] = order
-                queue.append(E[edge])
 
-    # for i in range(1, len(V)):
-    #     print(V[i])
+        for _ in range(len(queue)):
+            u = queue.popleft()
+            u.sort()    # 오름차순 방문 설정 
+            for edge in u:
+                if V[edge] == 0:
+                    order += 1
+                    V[edge] = 1
+                    D[edge] = depth
+                    O[edge] = order
+                    queue.append(E[edge])
 
+
+# 입력값 처리
 N, M, R = map(int, input().split())
-E = [[] for _ in range(N + 1)]  # 간선 정보
-V = [-1 for _ in range(N + 1)]  # 노드 깊이
-O = [0 for _ in range(N + 1)]   # 방문 순서
+E = [[] for _ in range(N + 1)]       # 간선 정보
+V = [0 for _ in range(N + 1)]        # 방문 정보
+D = [-1 for _ in range(N + 1)]       # 깊이 정보
+O = [0 for _ in range(N + 1)]        # 순서 정보
 
 for i in range(1, M + 1):
     u, v = map(int, input().split())
     E[u].append(v)
     E[v].append(u)
 
-bfs(V, E, R, O)
+# bfs 탐색 시작
+bfs(V, E, R, D, O)
 
+# 정답 출력
 answer = 0
 for i in range(1, N + 1):
-    answer += V[i] * O[i]
+    answer += D[i] * O[i]
 print(answer)
+# print(O)
+# print(D)
