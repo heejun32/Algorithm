9d706005311f0adc6b538c430741a8d918f7ff3e
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-06 12:21:59 +0900

    Time: 138 ms (24.56%), Space: 13.9 MB (90.11%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,18 +1,19 @@
 from collections import deque
 
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         origin_color = image[sr][sc]
         row, col = len(image), len(image[0])
-        
+
         if origin_color != color:
             queue = deque()
             queue.append([sr, sc])
-            
+
             while queue:
                 x, y = queue.popleft()
                 image[x][y] = color
-                
+
                 for nx, ny in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
                     if 0 <= nx < row and 0 <= ny < col and image[nx][ny] == origin_color:
                         queue.append((nx, ny))
