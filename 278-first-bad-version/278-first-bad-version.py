08053d232fb3402f251e81cda79ebb5ecfd9230a
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-07 11:31:59 +0900

    Time: 33 ms (86.72%), Space: 13.9 MB (12.61%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -5,14 +5,10 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         start = 1
         end = n
-        
         while start <= end:
             middle = (start + end) // 2
             if isBadVersion(middle):
                 end = middle - 1
             else:
                 start = middle + 1
-        
         return start
-                
-        
