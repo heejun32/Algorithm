18c184af8902740bd70fec0103cb0503bff8a8ae
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-08 09:37:05 +0900

    Time: 38 ms (72.20%), Space: 13.9 MB (57.14%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,9 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        if n <= 2:
+            return n
         
         dp = [0] * n
         dp[0] = 1
