0d5da9379501ec99ec4406a4d5cd8dcb1ffa1c67
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-08 09:30:44 +0900

    Time: 76 ms (67.19%), Space: 14.2 MB (18.33%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,14 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dict_maz = dict()
-        for s in magazine:
-            dict_maz[s] = dict_maz.get(s, 0) + 1
-        for s in ransomNote:
-            if s not in dict_maz:
+        dict_ran = collections.Counter(ransomNote)
+        dict_maz = collections.Counter(magazine)
+        for key, value in dict_ran.items():
+            if key not in dict_maz or value > dict_maz[key]:
                 return False
-            if dict_maz[s] == 0:
-                return False
-            dict_maz[s] -= 1
             
         return True
-        
