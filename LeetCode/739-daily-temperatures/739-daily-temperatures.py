80c9fce8e71171df892fcaab79232524cd1ad599
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-14 18:32:50 +0900

    Time: 1868 ms (41.72%), Space: 24.6 MB (84.69%) - LeetHub

---
+++ LeetCode/739-daily-temperatures/739-daily-temperatures.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        
+        for i, current in enumerate(temperatures):
+            # 현재 온도가 스택 값보다 높다면 정답 처리
+            while stack and current > temperatures[stack[-1]]:
+                last = stack.pop()
+                answer[last] = i - last
+            stack.append(i)
+        
+        return answer
+            
