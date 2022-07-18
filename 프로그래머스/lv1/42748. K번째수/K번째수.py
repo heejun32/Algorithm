6d571f7b599d8426c9f972cd400d10759fe7db2f
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-18 20:00:39 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,11 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i = command[0]
+        j = command[1]
+        k = command[2]
+        temp = sorted(array[i - 1: j])
+        answer.append(temp[k - 1])
+
+    return answer
