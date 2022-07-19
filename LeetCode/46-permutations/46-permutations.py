8ebba22eb40363e46514cc84929a45128aabe64c
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-19 16:09:39 +0900

    Time: 64 ms (45.37%), Space: 14 MB (83.89%) - LeetHub

--- LeetCode/46-permutations/46-permutations.py
+++ LeetCode/46-permutations/46-permutations.py
@@ -1,19 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(comb):
-            if len(comb) == len(nums):
-                result.append(comb)
-                return
-            for idx, num in enumerate(nums):
-                if not visited[idx]:
-                    visited[idx] = True
-                    comb.append(num)
-                    dfs(comb[:])
-                    visited[idx] = False
-                    comb.pop()
+#         def dfs(comb):
+#             if len(comb) == len(nums):
+#                 result.append(comb)
+#                 return
+#             for idx, num in enumerate(nums):
+#                 if not visited[idx]:
+#                     visited[idx] = True
+#                     comb.append(num)
+#                     dfs(comb[:])
+#                     visited[idx] = False
+#                     comb.pop()
 
-        visited = [False] * len(nums)
-        result = []
-        dfs([])
-        return result
-        
+#         visited = [False] * len(nums)
+#         result = []
+#         dfs([])
+#         return result
+        return list(map(list, itertools.permutations(nums)))
