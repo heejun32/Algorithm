886be31ab2509fea2add557a2b88b807f3838e58
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-20 10:53:37 +0900

    Time: 46 ms (67.34%), Space: 14.1 MB (33.99%) - LeetHub

--- LeetCode/78-subsets/78-subsets.py
+++ LeetCode/78-subsets/78-subsets.py
@@ -1,13 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def dfs(subset, index):
-            for i in range(index, len(nums)):
-                subset.append(nums[i])
-                results.append(subset[:])
-                dfs(subset[:], i + 1)
-                subset.pop()
-            return
+        results = []
         
-        results = [[]]
-        dfs([], 0)
+        def dfs(index, path):
+            results.append(path)
+            
+            for i in range(index, len(nums)):
+                dfs(i + 1, path + [nums[i]])
+            
+        dfs(0, [])
         return results
