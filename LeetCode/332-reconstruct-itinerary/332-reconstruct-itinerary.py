96ded2265bed95d731d9c872f774cfab8cb14d9b
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-20 11:32:59 +0900

    Time: 83 ms (94.34%), Space: 14.4 MB (68.39%) - LeetHub

--- LeetCode/332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ LeetCode/332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -1,17 +1,14 @@
-from collections import deque
-
-
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        graph = collections.defaultdict(deque)
+        graph = collections.defaultdict(list)
         # 그래프 구성
-        for a, b in sorted(tickets):
+        for a, b in sorted(tickets, reverse=True):
             graph[a].append(b)
         
         route = []
         def dfs(a):
             while graph[a]:
-                dfs(graph[a].popleft())
+                dfs(graph[a].pop())
             route.append(a)
             
         dfs('JFK')
