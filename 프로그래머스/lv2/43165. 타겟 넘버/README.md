3c93d9db750bdefdf48daef20ec6f2223002c4e7
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-07-27 00:38:51 +0900

    [level 2] Title: 타겟 넘버, Time: 1.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.36 ms
+메모리: 10.4 MB, 시간: 1.26 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,15 @@
 def solution(numbers, target):
     answer = 0
-    stack = []
-    stack.append([numbers[0], 0])
-    stack.append([-1 * numbers[0], 0])
-    n = len(numbers)
+    result = [0]
     
-    while (stack):
-        value, idx = stack.pop()
-        idx += 1
+    for num in numbers:
+        temp = []
+        for r in result:
+            temp.append(r + num)
+            temp.append(r - num)
+        result = temp
         
-        if idx < n:
-            stack.append([value + numbers[idx], idx])
-            stack.append([value + (-1 * numbers[idx]), idx])
-            
-        else:
-            if target == value:
-                answer += 1
-    
+    for r in result:
+        if r == target:
+            answer += 1
     return answer
