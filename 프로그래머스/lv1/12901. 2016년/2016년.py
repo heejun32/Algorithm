61c293823b517550f56fe02794aef85738715adc
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-04 01:54:04 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    calender = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 
+                7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    weekends = {0: 'THU', 1: 'FRI', 2: 'SAT', 3: 'SUN',
+                4: 'MON', 5: 'TUE', 6: 'WED'}
+    
+    days = 0
+    for i in range(1, a):
+        days += calender[i]
+    days += b
+
+    return weekends[days % 7]
