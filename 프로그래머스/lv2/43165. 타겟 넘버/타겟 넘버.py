75d240b12a277eea2e202741bbc3d4ec21c97a39
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-04 15:29:50 +0900

    [level 2] Title: 타겟 넘버, Time: 1.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,11 @@
 def solution(numbers, target):
-    answer = 0
     result = [0]
-    
+
     for num in numbers:
         temp = []
         for r in result:
             temp.append(r + num)
             temp.append(r - num)
         result = temp
-        
-    for r in result:
-        if r == target:
-            answer += 1
-    return answer
+
+    return result.count(target)
