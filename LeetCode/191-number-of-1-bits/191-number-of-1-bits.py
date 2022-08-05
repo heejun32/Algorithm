a2ab2f72f4ee5e2b9d9da893b22e3c0172ce3c11
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-05 17:02:25 +0900

    Time: 25 ms (67.14%), Space: 13.8 MB (10.97%) - LeetHub

--- LeetCode/191-number-of-1-bits/191-number-of-1-bits.py
+++ LeetCode/191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def hammingWeight(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        return bin(n).count('1')
-        
+        count = 0
+        while n:
+            n &= n - 1
+            count += 1
+        return count
