e851f01a1b1f67ccdb761b9ff24d9629ad1e9312
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-14 01:20:49 +0900

    Time: 24 ms (68.54%), Space: 13.3 MB (87.46%) - LeetHub

---
+++ LeetCode/119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        results = [[1]]
+        for i in range(2, rowIndex + 2):
+            rows = [1] * i
+            for j in range(1, i - 1):
+                rows[j] = results[-1][j - 1] + results[-1][j]
+            results.append(rows)
+        
+        return results[-1]
