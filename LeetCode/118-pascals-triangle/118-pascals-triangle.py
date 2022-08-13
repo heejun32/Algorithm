4dd3acb8738123ca878419c2d5d3b1b38abf4154
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-14 01:13:48 +0900

    Time: 23 ms (68.04%), Space: 13.2 MB (96.94%) - LeetHub

--- LeetCode/118-pascals-triangle/118-pascals-triangle.py
+++ LeetCode/118-pascals-triangle/118-pascals-triangle.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def generate(self, numRows):
-        results = []
-        for i in range(1, numRows + 1):
+        results = [[1]]
+        for i in range(2, numRows + 1):
             # 각 층 리스트 초기화
-            rows = []
-            for j in range(i):
-                if j == 0 or j == i - 1:
-                    rows.append(1)
-                    continue
-                # print(i, j)
-                rows.append(results[i - 2][j - 1] + results[i - 2][j])
+            rows = [1] * i
+            for j in range(1, i - 1):
+                rows[j] = results[-1][j - 1] + results[-1][j]
             results.append(rows)
         return results
             
