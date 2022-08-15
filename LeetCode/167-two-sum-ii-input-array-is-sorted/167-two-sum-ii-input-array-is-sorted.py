f79db55b76f1fe9c79101ef43a2c9a445142afc3
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-15 18:16:10 +0900

    Time: 264 ms (5.05%), Space: 14.8 MB (10.53%) - LeetHub

--- LeetCode/167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ LeetCode/167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,14 +1,18 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for k, v in enumerate(numbers):
-            left, right = k + 1, len(numbers) - 1
-            expected = target - v
-            
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(numbers)):
+            rested = target - numbers[i]
+            left, right = i + 1, len(numbers) - 1
             while left <= right:
                 mid = left + (right - left) // 2
-                if numbers[mid] < expected:
+                if numbers[mid] < rested:
                     left = mid + 1
-                elif numbers[mid] > expected:
+                elif numbers[mid] > rested:
                     right = mid - 1
                 else:
-                    return k + 1, mid + 1
+                    return [i + 1, mid + 1]
