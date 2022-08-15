0ac54b7c44452bba20c2d1946c2231b4b8e31e4a
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-15 18:21:27 +0900

    Time: 232 ms (43.06%), Space: 19.4 MB (69.37%) - LeetHub

--- LeetCode/240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ LeetCode/240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,7 +1,12 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row, col = 0, len(matrix[0]) - 1
-        
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = 0
+        col = len(matrix[0]) - 1
         while row <= len(matrix) - 1 and col >= 0:
             if target == matrix[row][col]:
                 return True
@@ -10,4 +15,3 @@ class Solution:
             elif target > matrix[row][col]:
                 row += 1
         return False
-        
