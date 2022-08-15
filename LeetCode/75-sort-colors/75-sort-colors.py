dfb0a186d693b2b6c058053a5e7030e74a9568d8
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-15 16:57:22 +0900

    Time: 28 ms (62.39%), Space: 13.5 MB (39.36%) - LeetHub

--- LeetCode/75-sort-colors/75-sort-colors.py
+++ LeetCode/75-sort-colors/75-sort-colors.py
@@ -1,18 +1,7 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        red, white, blue = 0, 0, len(nums)
-        
-        while white < blue:
-            if nums[white] < 1:
-                nums[red], nums[white] = nums[white], nums[red]
-                white += 1
-                red += 1
-            elif nums[white] > 1:
-                blue -= 1
-                nums[white], nums[blue] = nums[blue], nums[white]
-            else:
-                white += 1
-                
+        return nums.sort()
