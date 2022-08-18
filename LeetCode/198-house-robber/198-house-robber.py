d23dd2880dfc18fe2452095e3822e3219bc6c22e
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-18 17:07:32 +0900

    Time: 28 ms (51.88%), Space: 13.5 MB (17.07%) - LeetHub

--- LeetCode/198-house-robber/198-house-robber.py
+++ LeetCode/198-house-robber/198-house-robber.py
@@ -12,8 +12,10 @@ class Solution(object):
         dp = [0] * len(nums)
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
+        answer = float('-inf')
         
         for i in range(2, len(nums)):
             dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+            answer = max(answer, dp[i])
         return max(dp)
             
