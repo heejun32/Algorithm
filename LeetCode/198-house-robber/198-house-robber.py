047c0ec6c701e39220d9de4bd3bb4eba7524aa26
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-18 17:10:50 +0900

    Time: 30 ms (42.82%), Space: 13.6 MB (17.07%) - LeetHub

--- LeetCode/198-house-robber/198-house-robber.py
+++ LeetCode/198-house-robber/198-house-robber.py
@@ -8,14 +8,10 @@ class Solution(object):
         if len(nums) <= 2:
             return max(nums)
         
-        # 초기갑 설정
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        answer = float('-inf')
+        dp = collections.OrderedDict()
+        dp[0],dp[1] = nums[0], max(nums[0], nums[1])
         
         for i in range(2, len(nums)):
             dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
-            answer = max(answer, dp[i])
-        return answer
+        return dp.popitem()[1]
             
