842f55faef282c6245d1269736d2f65f816638ad
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-22 12:18:17 +0900

    Time: 190 ms (94.76%), Space: 20.8 MB (61.59%) - LeetHub

--- LeetCode/238-product-of-array-except-self/238-product-of-array-except-self.py
+++ LeetCode/238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,16 +1,19 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out = []
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         
+        out = []
         p = 1
         for i in range(len(nums)):
             out.append(p)
-            p *= nums[i]
+            p = p * nums[i]
         
         p = 1
-        for i in range(len(nums) -1, -1, -1):
-            out[i] *= p
-            p *= nums[i]
-            
-        return out
+        for i in range(len(nums) - 1, -1, -1):
+            out[i] = out[i] * p
+            p = p * nums[i]
         
+        return out
