e7f02e398029b0016a738b6821341ec58c4e9e86
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-23 14:34:04 +0900

    Time: 24 ms (71.47%), Space: 13.8 MB (12.62%) - LeetHub

--- LeetCode/20-valid-parentheses/20-valid-parentheses.py
+++ LeetCode/20-valid-parentheses/20-valid-parentheses.py
@@ -8,12 +8,16 @@ class Solution(object):
         stack = []
         
         for character in s:
+            # stack이 비어있지 않고, key 값의 괄호가 들어오면 비교
             if stack and character in brackets:
+                # 다를 경우 not valid pare, return False
                 if brackets[character] != stack[-1]:
                     return False
+                # 같을 경우 stack의 마지막 원소 pop뒤 다음 원소 확인
                 else:
                     stack.pop()
                     continue
             stack.append(character)
     
+        # 모든 탐새을 마치고 스택이 비어있는지 확인
         return len(stack) == 0
