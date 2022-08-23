8d53bf3346574fc21b09f9ddb11b3c839cc907c9
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-23 15:00:58 +0900

    Time: 25 ms (87.50%), Space: 13.5 MB (71.88%) - LeetHub

--- LeetCode/316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ LeetCode/316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,19 +1,24 @@
-from collections import Counter
+import collections
 
 
-class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        counter, stack, seen = Counter(s), list(), set()
-        
-        # start searching
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack, seen, counter = list(), set(), collections.Counter(s)
         for char in s:
             counter[char] -= 1
+            
             if char in seen:
                 continue
-
+            
             while stack and char < stack[-1] and counter[stack[-1]] > 0:
                 seen.remove(stack.pop())
+            
             stack.append(char)
             seen.add(char)
-
+        
         return ''.join(stack)
+        
