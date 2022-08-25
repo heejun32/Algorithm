1b11c0c170b4e33998ea587c32697f904739f7d2
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-25 23:04:06 +0900

    Time: 32 ms (92.56%), Space: 14 MB (32.00%) - LeetHub

--- LeetCode/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ LeetCode/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -2,20 +2,23 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         def dfs(index, path):
             if len(path) == len(digits):
-                result.append(path)
-                return
+                results.append(path)
+                return None
             
             for i in range(index, len(digits)):
-                for j in dic[digits[i]]:
+                for j in mapping_digits[digits[i]]:
                     dfs(i + 1, path + j)
-        
-        # 예외 처리
+                    
+        # 예외처리
         if not digits:
             return []
         
-        dic = {"2": "abc", "3": "def", "4": "ghi",
-               "5": "jkl", "6": "mno", "7": "pqrs",
-               "8": "tuv", "9": "wxyz"}
-        result = []
+        mapping_digits = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"],
+                  "5": ["j", "k", "l"], "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"],
+                  "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
+        
+        results = []
         dfs(0, "")
-        return result
+        
+        return results
+    
