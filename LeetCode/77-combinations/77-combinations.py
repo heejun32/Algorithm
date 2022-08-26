37693fcb7bb6a4fcc8aefc477cf50a7bf2cd8869
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-26 12:26:52 +0900

    Time: 97 ms (96.01%), Space: 15.7 MB (97.13%) - LeetHub

--- LeetCode/77-combinations/77-combinations.py
+++ LeetCode/77-combinations/77-combinations.py
@@ -1,16 +1,6 @@
+import itertools
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-#         def dfs(start, comb):
-#             if len(comb) == k:
-#                 result.append(comb)
-#                 return
-            
-#             for number in range(start, n + 1):
-#                 comb.append(number)
-#                 dfs(number + 1, comb[:])
-#                 comb.pop()
-        
-#         result = []
-#         dfs(1, [])
-#         return result
-        return list(map(list, itertools.combinations(range(1, n + 1), k)))
+        return list(itertools.combinations(range(1, n + 1), k))
