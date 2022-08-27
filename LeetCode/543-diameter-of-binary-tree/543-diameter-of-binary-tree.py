9b62eeec4f4dcbbc5c571b2eef53354030c951af
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-27 18:19:38 +0900

    Time: 85 ms (28.66%), Space: 16.2 MB (84.90%) - LeetHub

--- LeetCode/543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ LeetCode/543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -5,19 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    longest: int = 0
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        def dfs(node: TreeNode) -> int:
+    longest = 0
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
             if not node:
                 return -1
-            # 왼쪽, 오른쪽 각각 리프 노드까지 탐색
+            
             left = dfs(node.left)
             right = dfs(node.right)
-
-            # 가장 긴 경로
+            
             self.longest = max(self.longest, left + right + 2)
-            # 상태값
             return max(left, right) + 1
-
+        
         dfs(root)
         return self.longest
