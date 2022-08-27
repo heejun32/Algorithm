4aef4f69615730ef3878d8c2268664602101b592
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-27 22:39:16 +0900

    Time: 538 ms (5.03%), Space: 18.1 MB (13.99%) - LeetHub

--- LeetCode/529-minesweeper/529-minesweeper.py
+++ LeetCode/529-minesweeper/529-minesweeper.py
@@ -1,8 +1,8 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         def dfs(x, y):
-            if board[x][y] != "E":
-                return None
+            # if board[x][y] != "E":
+            #     return None
         
             # search near mine        
             count = 0
@@ -13,6 +13,7 @@ class Solution:
             if count:
                 board[x][y] = str(count)
             else:
+                # recurssive dfs
                 board[x][y] = "B"
                 for dx, dy in((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1), (x - 1, y - 1)):
                     if 0 <= dx <= len(board) - 1 and 0 <= dy <= len(board[0]) - 1 and board[dx][dy] == "E":
