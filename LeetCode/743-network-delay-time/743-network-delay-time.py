5cd8430dc6a649d95623d21c1759e950a677d74a
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-30 14:35:59 +0900

    Time: 630 ms (65.50%), Space: 16.7 MB (25.08%) - LeetHub

--- LeetCode/743-network-delay-time/743-network-delay-time.py
+++ LeetCode/743-network-delay-time/743-network-delay-time.py
@@ -6,22 +6,23 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = collections.defaultdict(list)
         dist = collections.defaultdict(int)
-    
+        
+        # 소요 시간, 정점
+        Q = [(0, k)]
+
         # 그래프 인접 리스트 구현
         for u, v, w in times:
             graph[u].append((v, w))
-        
-    
-        Q = [(0, k)]
-        # 우선순위 큐 최솟값 기준으로 정점까지 최단 경로 삽입
+
         while Q:
             time, node = heapq.heappop(Q)
             if node not in dist:
                 dist[node] = time
                 for v, w in graph[node]:
                     alt = time + w
-                    heapq.heappush(Q, (alt, v))
+                    heapq.heappush(Q, ((alt, v)))
 
+        # 결과 확인
         if len(dist) == n:
             return max(dist.values())
         return -1
