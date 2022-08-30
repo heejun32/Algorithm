ad43032d6346d1b7b0b8a9d17dd627ab1e1e5f8b
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-30 16:13:33 +0900

    Time: 103 ms (98.70%), Space: 15.3 MB (53.03%) - LeetHub

--- LeetCode/787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ LeetCode/787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -15,13 +15,18 @@ class Solution:
         # (가격, 정점, 경유지 횟수)
         Q = [(0, src, k)]
         
+        # 큐 연산: 소요시간이 제일 작은 경로를 계속해서 탐색한다.
         while Q:
             price, node, k = heapq.heappop(Q)
             if node == dst:
                 return price
+            # 비용이 적고(큐 연산), 이미 방문한 경로는 탐색에서 제외한다.
             if visited[node] >= k:
                 continue
+            # 그렇지 않다면 탐색 경로에 추가한다.(전체 최소 비용이 아닌 k 경로 횟수 내에서의 최소 비용을 탐색하는 것이다.)
+
             visited[node] = k
+            # print(visited)
             for v, w in graph[node]:
                 alt = price + w
                 heapq.heappush(Q, (alt, v, k - 1))
