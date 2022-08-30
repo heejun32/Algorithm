94ddf936443e825ca17dbb0e82409de04c76c58a
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-30 15:30:05 +0900

    Time: 139 ms (85.61%), Space: 15.3 MB (37.62%) - LeetHub

--- LeetCode/787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ LeetCode/787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -4,24 +4,24 @@ import heapq
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        
         # 그래프 인접 리스트로 구현
+        graph = collections.defaultdict(list)
         for f, t, p in flights:
             graph[f].append((t, p))
         
-        vis = [0] * n
+        # 방문 기록
+        visited = [-1] * n
         
-        # (가격, 정점, 남은 경유지 횟수)
-        Q = [(0, src, k + 1)]
+        # (가격, 정점, 경유지 횟수)
+        Q = [(0, src, k)]
         
         while Q:
             price, node, k = heapq.heappop(Q)
             if node == dst:
                 return price
-            if vis[node] >= k:
+            if visited[node] >= k:
                 continue
-            vis[node ] = k    
+            visited[node] = k
             for v, w in graph[node]:
                 alt = price + w
                 heapq.heappush(Q, (alt, v, k - 1))
