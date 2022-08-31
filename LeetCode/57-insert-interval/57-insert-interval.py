d5add00ef68ad982871d8ce0a722fea9039dd491
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-08-31 15:06:13 +0900

    Time: 145 ms (31.43%), Space: 17.3 MB (52.60%) - LeetHub

--- LeetCode/57-insert-interval/57-insert-interval.py
+++ LeetCode/57-insert-interval/57-insert-interval.py
@@ -1,18 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals.sort()
-        print(intervals)
-        
         merged = []
         for interval in intervals:
-            if merged:
-                # not overlapping
-                if merged[-1][1] >= interval[0]:
-                    merged[-1][1] = max(merged[-1][1], interval[1])
-                else:
-                    merged.append(interval)   
-            else:
+            if interval[1] < newInterval[0]:
                 merged.append(interval)
-                
+            elif interval[0] > newInterval[1]:
+                merged.append(newInterval)
+                newInterval = interval
+            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+        merged.append(newInterval)
         return merged
