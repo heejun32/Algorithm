5cff70e386b124b0269304bde9a81875ccaa2309
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-02 15:06:13 +0900

    Time: 1684 ms (36.92%), Space: 18 MB (71.62%) - LeetHub

--- LeetCode/15-3sum/15-3sum.py
+++ LeetCode/15-3sum/15-3sum.py
@@ -1,33 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # to use tow pointers
         nums.sort()
         
         results = []
         for i in range(len(nums) - 2):
-            # 중복된 값 건너뛰기
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
             left, right = i + 1, len(nums) - 1
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
-                if total < 0:
-                    left += 1
-                elif total > 0:
+                if total > 0 :
                     right -= 1
+                elif total < 0:
+                    left += 1
                 else:
-                    # 정답 기록
                     results.append([nums[i], nums[left], nums[right]])
                     
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
-                    
                     left += 1
                     right -= 1
-        
+            
         return results
-            
-            
