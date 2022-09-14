1c7511ae6c267533eb7811194fccb0f755b1460c
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-14 20:19:55 +0900

    Time: 730 ms (14.24%), Space: 17.4 MB (36.52%) - LeetHub

--- LeetCode/200-number-of-islands/200-number-of-islands.py
+++ LeetCode/200-number-of-islands/200-number-of-islands.py
@@ -1,25 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i, j):
-            # 더 이상 땅이 아닌 경우 종료
-            if i < 0 or i >= len(grid) or \
-                j < 0 or j >= len(grid[0]) or \
-                grid[i][j] != "1":
+        def dfs(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == "0":
                 return None
-            
-            grid[i][j] = "0"
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-
+            grid[x][y] = "0"
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
         
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    dfs(i, j)
-                    count += 1
         
-        return count
-                
+        m, n = len(grid), len(grid[0])
+        islands = 0
+        for x in range(m):
+            for y in range(n):
+                if grid[x][y] == "1":
+                    dfs(x, y)
+                    islands += 1
+        print(grid)
+        return islands
