9962491d4cb30d1444871c41411a29aeb3111ce2
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-14 17:39:45 +0900

    Time: 215 ms (22.69%), Space: 15.6 MB (71.55%) - LeetHub

---
+++ LeetCode/210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,29 @@
+import collections
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = collections.defaultdict(list)
+        queue = collections.deque()
+        indegree = [0] * numCourses
+        answer = []
+        
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        while queue:
+            course = queue.popleft()
+            answer.append(course)
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+        
+        return answer if len(answer) == numCourses else []
+        
+            
