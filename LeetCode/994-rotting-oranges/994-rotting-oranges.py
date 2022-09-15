38e6eaf4b6d9aed3aba2efa78fd398e03e56327f
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-15 12:09:48 +0900

    Time: 55 ms (92.28%), Space: 13.9 MB (47.79%) - LeetHub

--- LeetCode/994-rotting-oranges/994-rotting-oranges.py
+++ LeetCode/994-rotting-oranges/994-rotting-oranges.py
@@ -8,7 +8,6 @@ class Solution:
         Space Complexity is O(MN)
 
         '''
-        
         def bfs(fo: int, ro: List[List[int]]) -> int:
             #exception
             if not fo:
@@ -16,8 +15,10 @@ class Solution:
                 
             time = -1
             while ro:
+                # search the same level
                 for _ in range(len(ro)):
                     x, y = ro.popleft()
+                    # search the 4-directionally adjacent to the oranges
                     for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
                         if 0 <= dx < m and 0 <= dy < n and grid[dx][dy] == 1:
                             fo -= 1
@@ -26,6 +27,7 @@ class Solution:
                 time += 1
             return time if not fo else -1
         
+        # initialize the parameters: m, n, fresh_oranges, rotten_oranges
         m, n = len(grid), len(grid[0])
         fo = 0
         ro = collections.deque()
