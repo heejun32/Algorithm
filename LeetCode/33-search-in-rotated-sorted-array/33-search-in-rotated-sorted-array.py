605539ea656747ffcf089f7aa0d069758c756bb6
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-15 14:37:35 +0900

    Time: 114 ms (5.25%), Space: 14.2 MB (55.52%) - LeetHub

--- LeetCode/33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ LeetCode/33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,34 +1,28 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # 최솟값 index 찾기
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # finding pivot
         left, right = 0, len(nums) - 1
         while left < right:
             mid = left + (right - left) // 2
+            
             if nums[mid] > nums[right]:
                 left = mid + 1
             else:
                 right = mid
-        
-        # 최솟값 index 위치가 pivot
+                
         pivot = left
+        
+        # finding target
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + (right - left) // 2
-            pivot_mid = (mid + pivot) % len(nums)
+            mid_pivot = (mid + pivot) % len(nums)
             
-            if nums[pivot_mid] < target:
+            if nums[mid_pivot] < target:
                 left = mid + 1
-            elif nums[pivot_mid] > target:
+            elif nums[mid_pivot] > target:
                 right = mid - 1
             else:
-                return pivot_mid
+                return mid_pivot
         
-        # 결과가 없다면
         return -1
-                
-        
