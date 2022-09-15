56ec404c51328f240603a35cd96300375748e289
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-15 11:46:48 +0900

    Time: 60 ms (85.40%), Space: 14 MB (47.79%) - LeetHub

---
+++ LeetCode/994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,43 @@
+import collections
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        def bfs(fo: int, ro: List[List[int]]) -> int:
+            #exception
+            if not fo:
+                return 0
+                
+            time = -1
+            
+            while ro:
+                # debugging
+                print(ro)
+
+                for _ in range(len(ro)):
+                    x, y = ro.popleft()
+                    for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                        if 0 <= dx < m and 0 <= dy < n and grid[dx][dy] == 1:
+                            fo -= 1
+                            grid[dx][dy] = 2
+                            ro.append([dx, dy])
+                time += 1
+            
+            
+            return time if not fo else -1
+        
+        
+        
+        
+        m, n = len(grid), len(grid[0])
+        fo = 0
+        ro = collections.deque()
+        
+        for x in range(m):
+            for y in range(n):
+                if grid[x][y] == 1:
+                    fo += 1
+                if grid[x][y] == 2:
+                    ro.append([x, y])
+        
+        return bfs(fo, ro)
