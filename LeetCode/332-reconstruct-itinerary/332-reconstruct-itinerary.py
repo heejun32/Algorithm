2d8b790666c6f87b05e29e133bd4fcbac8c74770
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 20:16:15 +0900

    Time: 84 ms (95.50%), Space: 14.4 MB (89.31%) - LeetHub

--- LeetCode/332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ LeetCode/332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -3,19 +3,16 @@ import collections
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def dfs(a):
+            while graph[a]:
+                dfs(graph[a].pop())
+            answer.append(a)
         
-        def dfs(start):
-            while graph[start]:
-                dfs(graph[start].pop())
-            rount.append(start)
-                
-        
-        # {출발지: [도착지1, 도착지2, ...]} 저장
         graph = collections.defaultdict(list)
-        for depart, arrive in sorted(tickets, reverse=True):
-            graph[depart].append(arrive)
- 
-        rount = []
-        dfs('JFK')
-        return rount[::-1]
+        for a, b in sorted(tickets, reverse=True):
+            graph[a].append(b)
+                    
+        answer = []
+        dfs("JFK")
         
+        return answer[::-1]
