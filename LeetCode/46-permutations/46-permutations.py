d3ad1d7d16c170c16b2891bc531541f298016509
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 19:24:23 +0900

    Time: 76 ms (29.70%), Space: 14 MB (83.92%) - LeetHub

--- LeetCode/46-permutations/46-permutations.py
+++ LeetCode/46-permutations/46-permutations.py
@@ -1,22 +1,4 @@
 class Solution:
-    '''
-    Time Complexity is O(N!)
-    Space Complexity is O(N!)
-    ''' 
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(path):
-            if len(path) == len(nums):
-                answer.append(path)
-                return None
-            
-            for i in range(len(nums)):
-                if not visited[i]:
-                    visited[i] = True
-                    dfs(path + [nums[i]])
-                    visited[i] = False
-                
-        answer = []
-        visited = [False] * len(nums)
-        dfs([])
-        
-        return answer
+        return itertools.permutations(nums)        
+        # return list(map(list, itertools.permutations(nums)))
