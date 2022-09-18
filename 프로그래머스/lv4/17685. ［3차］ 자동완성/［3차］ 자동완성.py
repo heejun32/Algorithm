fb227a51cba6f89183c0b0a9de3900350b928e94
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 18:24:16 +0900

    [level 4] Title: [3차] 자동완성, Time: 1959.88 ms, Memory: 414 MB -BaekjoonHub

--- 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
+++ 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
@@ -17,16 +17,17 @@ class Trie:
     
     def search(self, word: str) -> int:
         node = self
-        for idx, char in enumerate(word):
+        for idx, char in enumerate(word, 1):
             if node.children[char].count == 1:
                 break
             node = node.children[char]
 
-        return idx + 1
+        return idx
     
     def __str__(self):
-        return str(self.children)+' '+str(self.count)
+        return str(self.children) + ' ' + str(self.count)
 
+    
 def solution(words):
     answer = 0
     obj = Trie()
@@ -34,5 +35,6 @@ def solution(words):
         obj.insert(word)
     for word in words:
         answer += obj.search(word)
-
+    # debugging
+    # print(obj)
     return answer
