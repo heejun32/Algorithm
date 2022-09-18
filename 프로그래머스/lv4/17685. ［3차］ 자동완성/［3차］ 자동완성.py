efde2223db54bfcc842eaa8c7f5b2c6792ae76a3
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 18:14:39 +0900

    [level 4] Title: [3차] 자동완성, Time: 2113.16 ms, Memory: 414 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
@@ -0,0 +1,38 @@
+import collections
+
+
+class Trie:
+    
+    def __init__(self):
+        self.children = collections.defaultdict(Trie)
+        self.count = 0
+    
+    def insert(self, word: str) -> None:
+        node = self
+        for char in word:
+            if char not in node.children:
+                node.children[char] = Trie()
+            node = node.children[char]
+            node.count += 1
+    
+    def search(self, word: str) -> int:
+        node = self
+        for idx, char in enumerate(word):
+            if node.children[char].count == 1:
+                break
+            node = node.children[char]
+
+        return idx + 1
+    
+    def __str__(self):
+        return str(self.children)+' '+str(self.count)
+
+def solution(words):
+    answer = 0
+    obj = Trie()
+    for word in words:
+        obj.insert(word)
+    for word in words:
+        answer += obj.search(word)
+
+    return answer
