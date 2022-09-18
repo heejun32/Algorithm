8e2cb872bf5cb75babd68ea66e4f71799829c141
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 19:03:19 +0900

    Time: 46 ms (61.31%), Space: 13.9 MB (31.66%) - LeetHub

--- LeetCode/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ LeetCode/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,24 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def dfs(index, path):
-            if len(path) == len(digits):
-                results.append(path)
-                return None
-            
-            for i in range(index, len(digits)):
-                for j in mapping_digits[digits[i]]:
-                    dfs(i + 1, path + j)
-                    
-        # 예외처리
+        # exception
         if not digits:
             return []
         
-        mapping_digits = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"],
-                  "5": ["j", "k", "l"], "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"],
-                  "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
+        letters = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"], 
+                   "5": ["j", "k", "l"], "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"],
+                   "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
         
-        results = []
+        answer = []
+        
+        def dfs(idx, path):
+            if len(path) == len(digits):
+                answer.append(path)
+                return None
+            
+            for i in range(idx, len(digits)):
+                for char in letters[digits[i]]:
+                    dfs(i + 1, path + char)
+        
         dfs(0, "")
+        return answer
+            
         
-        return results
-    
