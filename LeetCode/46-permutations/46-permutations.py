f77cea1a0a4bd9bf833ee3d95fc28fa74ee669ed
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 19:17:07 +0900

    Time: 81 ms (20.55%), Space: 14.2 MB (22.80%) - LeetHub

--- LeetCode/46-permutations/46-permutations.py
+++ LeetCode/46-permutations/46-permutations.py
@@ -1,19 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(permu):
-            if len(permu) == len(nums):
-                result.append(permu)
-                return
-            for idx, num in enumerate(nums):
-                if not visited[idx]:
-                    visited[idx] = True
-                    permu.append(num)
-                    dfs(permu[:])
-                    visited[idx] = False
-                    permu.pop()
-
-                    
+        def dfs(path):
+            if len(path) == len(nums):
+                answer.append(path)
+                return None
+            
+            for i in range(len(nums)):
+                if not visited[i]:
+                    visited[i] = True
+                    dfs(path + [nums[i]])
+                    visited[i] = False
+                
+        answer = []
         visited = [False] * len(nums)
-        result = []
         dfs([])
-        return result
+        
+        return answer
