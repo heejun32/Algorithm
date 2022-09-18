ef190996a6733ea4de27dcd8d5aef861d6730299
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 19:07:23 +0900

    Time: 63 ms (15.41%), Space: 14 MB (31.66%) - LeetHub

--- LeetCode/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ LeetCode/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -9,13 +9,12 @@ class Solution:
         # exception
         if not digits:
             return []
-        
+
         letters = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"], 
                    "5": ["j", "k", "l"], "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"],
                    "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
-        
         answer = []
-        
+
         def dfs(idx, path):
             if len(path) == len(digits):
                 answer.append(path)
@@ -24,8 +23,7 @@ class Solution:
             for i in range(idx, len(digits)):
                 for char in letters[digits[i]]:
                     dfs(i + 1, path + char)
-        
+
         dfs(0, "")
+ 
         return answer
-            
-        
