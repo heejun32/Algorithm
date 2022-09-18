e856d35c4ea6aa9a22e94f3120a8c02a167bbbea
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-19 00:46:13 +0900

    Time: 326 ms (10.84%), Space: 16 MB (44.51%) - LeetHub

--- LeetCode/42-trapping-rain-water/42-trapping-rain-water.py
+++ LeetCode/42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,9 +1,12 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # exception
+        if len(height) == 1:
+            return 0
+        
         volume = 0
         left, right = 0, len(height) - 1
         left_max, right_max = height[left], height[right]
-        
         while left < right:
             left_max, right_max = max(left_max, height[left]), max(right_max, height[right])
             if left_max <= right_max:
@@ -12,4 +15,7 @@ class Solution:
             else:
                 volume += right_max - height[right]
                 right -= 1
+        
         return volume
+            
+            
