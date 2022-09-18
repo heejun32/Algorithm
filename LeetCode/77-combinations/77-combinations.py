09f29d4201bb00a58b5d0cb001d03686a79754e9
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-18 19:28:32 +0900

    Time: 1028 ms (13.70%), Space: 15.8 MB (85.41%) - LeetHub

--- LeetCode/77-combinations/77-combinations.py
+++ LeetCode/77-combinations/77-combinations.py
@@ -1,16 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def dfs(elements, start, k):
-            if k == 0:
-                results.append(elements[:])
+        def dfs(start, path):
+            if len(path) == k:
+                answer.append(path)
                 return None
             
             for i in range(start, n + 1):
-                elements.append(i)
-                dfs(elements, i + 1, k - 1)
-                elements.pop()
+                dfs(i + 1, path + [i])
+                
+        answer = []
+        dfs(1, [])
         
-        results = []
-        dfs([], 1, k)
-        
-        return results
+        return answer
