86ea35b6642abe4937447f3e3929b2ba11f980c2
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-20 09:00:24 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 57.35 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -6,11 +6,16 @@ def solution(queue1, queue2):
     Time Complexity is O(N)
     Space Complexity is O(N)
     '''
-    q1, q2 = collections.deque(queue1), collections.deque(queue2)
-    qsum_1, qsum_2 = sum(q1), sum(q2)
-    cnt = 0
+    qsum_1, qsum_2 = sum(queue1), sum(queue2)
     
-    # 종료 조건1
+    # exception
+    if (qsum_1 + qsum_2) % 2 == 1:
+        return -1
+    
+    q1, q2 = collections.deque(queue1), collections.deque(queue2)
+    cnt = 0
+
+    # 종료 조건1: 전체 탐색 범위를 다 봤을 때
     while cnt <= 3 * len(queue1):
         # 종료 조건2: 정답을 찾은 경우
         if qsum_1 == qsum_2: 
@@ -28,4 +33,5 @@ def solution(queue1, queue2):
         cnt += 1
         # debugging
         # print(q1, q2, cnt)
+
     return -1
