ca93f838c4a022e6753def51b22c011b123379ea
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-20 09:20:33 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,12 +1,18 @@
 def solution(brown, yellow):
-    s = brown + yellow
-    
-    for width in range(s, 0, -1):
-        height = s // width
-        y = (width - 2) * (height - 2)
-        b = s - y
-        if y == yellow and b == brown:
-            return [width, height]
-            break
-    
+    '''
+    (가로 - 2) * (세로 - 2) == 노란색 격자 수
+    가로 * 세로 - 노란색 격자 수 == 갈색 격자 수
+    '''
 
+    # 가로, 세로
+    answer = []
+    
+    for wide in range(5000):
+        for height in range(5000):
+            if wide < height:
+                break
+            y_r2 = (wide - 2) * (height - 2)             
+            if y_r2 == yellow and wide * height - y_r2 == brown:
+                return [wide, height]
+
+    return answer
