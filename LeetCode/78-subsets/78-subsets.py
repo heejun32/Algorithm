943488e2204aeafaf54cdce32e57fd81451fd082
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-20 10:16:23 +0900

    Time: 64 ms (30.40%), Space: 14.2 MB (35.77%) - LeetHub

--- LeetCode/78-subsets/78-subsets.py
+++ LeetCode/78-subsets/78-subsets.py
@@ -1,13 +1,16 @@
 class Solution:
+    '''
+    Time Complexity is O(2 ^ N)
+    Space Complexity is O(2 ^ N)
+
+    '''
     def subsets(self, nums: List[int]) -> List[List[int]]:
         def dfs(start, path):
             answer.append(path)
             
             for i in range(start, len(nums)):
-                path.append(nums[i])
-                dfs(i + 1, path[:])
-                path.pop()
-                
+                dfs(i + 1, path + [nums[i]])
+        
         answer = []
         dfs(0, [])
         
