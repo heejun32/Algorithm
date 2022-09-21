eec229a4843420302bd548a6cb3ab3ec4434db66
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-21 10:31:06 +0900

    Time: 769 ms (93.41%), Space: 72 MB (43.89%) - LeetHub

--- LeetCode/981-time-based-key-value-store/981-time-based-key-value-store.py
+++ LeetCode/981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,33 +1,29 @@
+import collections
+
+
 class TimeMap:
-    '''
-    Time Complexity
-        - Set: O(1) single operation, and total O(n).
-        - Get: O(log n) for a single operation, and total O(m log n).
-    Space Complexity is O(N)
-    '''
+
     def __init__(self):
-        self.time_map = defaultdict(list)
+        self.time_map = collections.defaultdict(list)       
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.time_map[key].append([timestamp, value])
+        self.time_map[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        # exception
-        if key not in self.time_map:
-            return ""
-        
-        array = self.time_map[key]
-        left, right = 0, len(array)
+        mini_map = self.time_map[key]
+        left, right = 0, len(mini_map)
         
         while left < right:
             mid = left + (right - left) // 2
-            if array[mid][0] <= timestamp:
+            
+            if mini_map[mid][0] <= timestamp:
                 left = mid + 1
             else:
                 right = mid
-
-        return "" if not right else array[right - 1][1]
         
+        return "" if not right else mini_map[right - 1][1]
+
+
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
