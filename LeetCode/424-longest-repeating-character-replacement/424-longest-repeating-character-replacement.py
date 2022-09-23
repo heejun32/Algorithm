7b64f701960a341dfbe91ccb14b22d134368e6b2
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-23 11:57:00 +0900

    Time: 1064 ms (5.01%), Space: 14 MB (18.63%) - LeetHub

--- LeetCode/424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ LeetCode/424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,19 +1,36 @@
+import collections
+
+
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         left = 0
+#         counter = collections.Counter()
+        
+#         for right in range(1, len(s) + 1):
+#             counter[s[right - 1]] += 1
+            
+#             # 가장 흔하게 등장하는 단어 탐색
+#             max_char_n = counter.most_common(1)[0][1]
+            
+#             if right - left - max_char_n > k:
+#                 counter[left] -= 1
+#                 left += 1
+        
+#         return right - left
+    
+
 class Solution(object):
     def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        # max(right) - max(left) - max_char_n = k 이면 정답
         left = 0
-        counter = collections.Counter()
+        counts = collections.Counter()
         for right in range(1, len(s) + 1):
-            counter[s[right - 1]] += 1
-            max_char_n = counter.most_common(1)[0][1]
+            counts[s[right - 1]] += 1
+            # 가장 흔하게 등장하는 문자 탐색
+            max_char_n = counts.most_common(1)[0][1]
             
+            # k 초과시 왼쪽 포인터 이동
             if right - left - max_char_n > k:
-                counter[s[left]] -= 1
+                counts[s[left]] -= 1
                 left += 1
+        
         return right - left
-            
