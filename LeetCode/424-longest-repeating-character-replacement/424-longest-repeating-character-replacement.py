bd2fc522bc63c392697ff92c5764d6d4437c1762
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-23 12:01:01 +0900

    Time: 490 ms (11.50%), Space: 14 MB (57.29%) - LeetHub

--- LeetCode/424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ LeetCode/424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,36 +1,36 @@
 import collections
 
 
-# class Solution:
-#     def characterReplacement(self, s: str, k: int) -> int:
-#         left = 0
-#         counter = collections.Counter()
-        
-#         for right in range(1, len(s) + 1):
-#             counter[s[right - 1]] += 1
-            
-#             # 가장 흔하게 등장하는 단어 탐색
-#             max_char_n = counter.most_common(1)[0][1]
-            
-#             if right - left - max_char_n > k:
-#                 counter[left] -= 1
-#                 left += 1
-        
-#         return right - left
-    
-
-class Solution(object):
-    def characterReplacement(self, s, k):
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
         left = 0
         counts = collections.Counter()
+        
         for right in range(1, len(s) + 1):
             counts[s[right - 1]] += 1
-            # 가장 흔하게 등장하는 문자 탐색
+            
+            # 가장 흔하게 등장하는 단어 탐색
             max_char_n = counts.most_common(1)[0][1]
             
-            # k 초과시 왼쪽 포인터 이동
             if right - left - max_char_n > k:
                 counts[s[left]] -= 1
                 left += 1
         
         return right - left
+    
+
+# class Solution(object):
+#     def characterReplacement(self, s, k):
+#         left = 0
+#         counts = collections.Counter()
+#         for right in range(1, len(s) + 1):
+#             counts[s[right - 1]] += 1
+#             # 가장 흔하게 등장하는 문자 탐색
+#             max_char_n = counts.most_common(1)[0][1]
+            
+#             # k 초과시 왼쪽 포인터 이동
+#             if right - left - max_char_n > k:
+#                 counts[s[left]] -= 1
+#                 left += 1
+        
+#         return right - left
