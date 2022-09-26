0cf5d128147c594c835a5f21b87a769ac45d8d01
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-26 11:52:55 +0900

    Time: 359 ms (36.46%), Space: 17 MB (81.71%) - LeetHub

--- LeetCode/324-wiggle-sort-ii/324-wiggle-sort-ii.py
+++ LeetCode/324-wiggle-sort-ii/324-wiggle-sort-ii.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums.sort()
-        half = len(nums[::2]) - 1
+        half = (len(nums) - 1) // 2
         nums[::2], nums[1::2] = nums[half::-1], nums[:half:-1]
         
         return None
