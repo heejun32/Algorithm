a6826da2599b62334bcc384224ff2de535a74bfd
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-26 13:01:23 +0900

    Time: 207 ms (11.55%), Space: 14.3 MB (80.37%) - LeetHub

---
+++ LeetCode/152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_pro = min_pro = answer = nums[0]
+        
+        for i in range(1, len(nums)):
+            max_pro, min_pro = max(nums[i], max_pro * nums[i], min_pro * nums[i]), min(nums[i], max_pro * nums[i], min_pro * nums[i])
+            answer = max(answer, max_pro)
+            
+        return answer
