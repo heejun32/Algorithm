ccbb8c6a5e3d829333f43cb9432316c692c67953
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-09-27 08:24:31 +0900

    [level 2] Title: 하노이의 탑, Time: 8.71 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,5 +1,10 @@
 from typing import List
 
+'''
+Time Complexity is O(2^n - 1)
+Space Complexity is O(2^n - 1)
+'''
+
 def solution(n: int) -> List[List[int]]:
     answer = []
     
@@ -7,10 +12,10 @@ def solution(n: int) -> List[List[int]]:
         if n == 1:
             answer.append([start, end])
             return None
-        else:
-            hanoi(n - 1, start, via, end)
-            answer.append([start, end])
-            hanoi(n - 1, via, end, start)
+
+        hanoi(n - 1, start, via, end)
+        answer.append([start, end])
+        hanoi(n - 1, via, end, start)
 
     hanoi(n, 1, 3, 2)
 
