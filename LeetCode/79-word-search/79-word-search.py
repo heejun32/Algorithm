7c702feda0a128fb753ba9b52be98a6bd231884c
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-10-04 12:11:36 +0900

    Time: 8953 ms (19.94%), Space: 14.1 MB (12.53%) - LeetHub

--- LeetCode/79-word-search/79-word-search.py
+++ LeetCode/79-word-search/79-word-search.py
@@ -1,35 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(x, y, k):
-            if x < 0 or x >= m or y < 0 or y >= n:
+            # 종료 조건: 탐색 범위를 넘은 경우, word 글자수보다 더 탐색한 경우, 이미 방문한 지점의 경우, 탐색하는 문자가 다른 경우
+            if x < 0 or x >= m or y < 0 or y >= n or k >= len(word) or (x, y) in visited or word[k] != board[x][y]:
                 return False
-            
-            if board[x][y] == '#':
-                return False
-            
-            if board[x][y] != word[k]:
-                return False
-            
-            if k == w:
+
+            if k == len(word) - 1:
                 return True
             
-            tmp = board[x][y]
-            board[x][y] = '#'
-            k += 1
-            
-            for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                if dfs(x + dx, y + dy, k):
+            visited.add((x, y))
+            for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                if dfs(dx, dy, k + 1):
                     return True
-            
-            board[x][y] = tmp
+            visited.remove((x, y))
             return False
         
-        m, n, w = len(board), len(board[0]), len(word) - 1
+        
+        m, n = len(board), len(board[0])
         visited = set()
         
         for row in range(m):
             for col in range(n):
-                if dfs(row, col, 0):
+                if board[row][col] == word[0] and dfs(row, col, 0):
                     return True
-                            
+                    
         return False
