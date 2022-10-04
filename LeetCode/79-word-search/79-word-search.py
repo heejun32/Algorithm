b7ab61cb281926a8f9317ef9365f51593f7a0a24
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-10-05 01:18:27 +0900

    Time: 3964 ms (88.11%), Space: 14.1 MB (12.53%) - LeetHub

--- LeetCode/79-word-search/79-word-search.py
+++ LeetCode/79-word-search/79-word-search.py
@@ -3,23 +3,24 @@ import collections
 
 class Solution:
     '''
-    Time Complexity is O(NM!)
-    Space Complexity is O(NM)
+    Time Complexity is O(MN3^L)
+    Space Complexity is O(1)
     '''
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(x, y, k):
             # 종료 조건: 탐색 범위를 넘은 경우, word 글자수보다 더 탐색한 경우, 이미 방문한 지점의 경우, 탐색하는 문자가 다른 경우
-            if x < 0 or x >= m or y < 0 or y >= n or k >= len(word) or (x, y) in visited or word[k] != board[x][y]:
+            if x < 0 or x >= m or y < 0 or y >= n or k >= len(word) or board[x][y] == '#' or word[k] != board[x][y]:
                 return False
 
             if k == len(word) - 1:
                 return True
             
-            visited.add((x, y))
+            mark = board[x][y]
+            board[x][y] = '#'
             for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
                 if dfs(dx, dy, k + 1):
                     return True
-            visited.remove((x, y))
+            board[x][y] = mark
             return False
         
         m, n = len(board), len(board[0])
