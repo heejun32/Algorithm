9941fec05ab3a033ae05a78c7e5834087e86d1dd
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-10-09 19:14:48 +0900

    Time: 112 ms (29.77%), Space: 14 MB (11.54%) - LeetHub

--- LeetCode/994-rotting-oranges/994-rotting-oranges.py
+++ LeetCode/994-rotting-oranges/994-rotting-oranges.py
@@ -3,43 +3,43 @@ import collections
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        '''
-        Time Complexity is O(MN)
-        Space Complexity is O(MN)
-
-        '''
-        def bfs(fo: int, ro: List[List[int]]) -> int:
-            #exception
-            if not fo:
-                return 0
-                
+        def bfs(rotten_oranges, fresh_oranges):
             time = 0
             
-            while ro:
-                # search the same level
-                for _ in range(len(ro)):
-                    x, y = ro.popleft()
-                    # search the 4-directionally adjacent to the oranges
+            # bfs
+            while rotten_oranges:
+                
+                # search the same level during 1 time
+                time += 1
+                for _ in range(len(rotten_oranges)):                
+                    x, y = rotten_oranges.popleft()
+                    
+                    # dx, dy must be in grid and fresh
                     for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
                         if 0 <= dx < m and 0 <= dy < n and grid[dx][dy] == 1:
-                            fo -= 1
                             grid[dx][dy] = 2
-                            ro.append([dx, dy])
-                            
-                if ro:
-                    time += 1
-            return time if not fo else -1
+                            fresh_oranges -= 1
+                            rotten_oranges.append((dx, dy))
+            
+            if fresh_oranges:
+                return -1
+            return time - 1
+            
         
-        # initialize the parameters: m, n, fresh_oranges, rotten_oranges
         m, n = len(grid), len(grid[0])
-        fo = 0
-        ro = collections.deque()
+        rotten_oranges = collections.deque()
+        fresh_oranges = 0
         
         for x in range(m):
             for y in range(n):
-                if grid[x][y] == 1:
-                    fo += 1
                 if grid[x][y] == 2:
-                    ro.append([x, y])
+                    rotten_oranges.append((x, y))
+                if grid[x][y] == 1:
+                    fresh_oranges += 1
         
-        return bfs(fo, ro)
+        # Exception
+        if not fresh_oranges:
+            return 0
+        
+        return bfs(rotten_oranges, fresh_oranges)
+        
