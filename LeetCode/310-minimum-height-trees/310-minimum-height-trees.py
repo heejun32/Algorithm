6192b7f6ad1add7b9d61ae5ea8e39ce167d72a40
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-10-10 14:14:26 +0900

    Time: 1422 ms (19.27%), Space: 24.5 MB (82.73%) - LeetHub

--- LeetCode/310-minimum-height-trees/310-minimum-height-trees.py
+++ LeetCode/310-minimum-height-trees/310-minimum-height-trees.py
@@ -1,28 +1,36 @@
+import collections
+
+
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n <= 1:
+        # Exception
+        if n == 1:
             return [0]
         
+        # 양방향 그래프 구성
         graph = collections.defaultdict(list)
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
-            
-        leaves = []
-        for i in range(n + 1):
-            if len(graph[i]) == 1:
-                leaves.append(i)
         
+        # 첫 번째 리프 노드 추가
+        leaves = []
+        for a in range(n):
+            if len(graph[a]) == 1:
+                leaves.append(a)
+                
+        # root 노트만 남을 때까지 반복 제거
         while n > 2:
             n -= len(leaves)
             new_leaves = []
+            
             for leaf in leaves:
                 neighbor = graph[leaf].pop()
                 graph[neighbor].remove(leaf)
                 
                 if len(graph[neighbor]) == 1:
                     new_leaves.append(neighbor)
+            
             leaves = new_leaves
             
-        
         return leaves
